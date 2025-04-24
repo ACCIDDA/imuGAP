ed4701cd76ee7529ca98d8ea51ef27e8f64affe6
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2025-04-24 09:56:49 -0400

    initial output of rstantools::rstan_create_package()

---
+++ R/gdmimes-package.R
@@ -0,0 +1,18 @@
+#' The 'gdmimes' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name gdmimes-package
+#' @aliases gdmimes
+#' @useDynLib gdmimes, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version NA. https://mc-stan.org
+#'
+NULL
